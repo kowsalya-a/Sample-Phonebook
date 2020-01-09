e8681220cac5905193dc432ef7694271b9fdefe0
Kowsalya A <akowsalya@pkrmsezdc.com>
2020-01-09 19:53:58 +0530

    first commit

--- WriteToFile.cs
+++ WriteToFile.cs
@@ -7,14 +7,29 @@ namespace Sample_Phonebook
     class WriteToFile{
         public void FileHandler(List<Contact> contacts )
         {
-           string data;
-           FileStream fileStream = new FileStream("D:\\Text.txt", FileMode.Open, FileAccess.Read);
-           using (StreamReader streamReader = new StreamReader(fileStream))
-           {
-                data = streamReader.ReadToEnd();
-           }
-           Console.WriteLine(data);
-           Console.ReadLine(); 
+           //string data;
+          // FileStream fileStream = new FileStream("D:\\Text.txt", FileMode.Open, FileAccess.Read);
+         //  using (StreamReader streamReader = new StreamReader(fileStream))
+        //   {
+        //        data = streamReader.ReadToEnd();
+         //  }
+         //  Console.WriteLine(data);
+         //  Console.ReadLine(); 
+
+            string path = @"C:\Users\akowsalya\text.txt";
+            
+            using (var writeToFile = new StreamWriter(path, true))
+            {
+                foreach (var item in contacts)
+                {
+                    string messageToBePrinted = string.Concat("FirstName: ", item.contactName, Environment.NewLine,   "PhoneNumber: ", item.contactNumber, Environment.NewLine, "Email: ", item.emailId);
+                    writeToFile.WriteLine(messageToBePrinted);
+                    
+                }
+            }
         }
-    }      
+        
+    }
 }
+
+
